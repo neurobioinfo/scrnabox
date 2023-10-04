2e4bb9840f65375b5eaa928671d032397546e0c1
fiorini9 <110110777+fiorini9@users.noreply.github.com>
2023-10-03 21:21:06 -0400

    Update test_code_Step1_hto.sh

--- test/test_code/test_code_Step1_hto.sh
+++ test/test_code/test_code_Step1_hto.sh
@@ -1,9 +1,8 @@
-cd /lustre04/scratch/fiorini9/scrna_pipeline/hto_v1.39/pipeline
+cd /lustre04/scratch/fiorini9/scrna_pipeline/test_code/pipeline_hto
 
-export SCRNABOX_HOME=/lustre04/scratch/fiorini9/scrna_pipeline/standard_v1.39/scrnabox0/scrnabox/scrnabox.slurm
-export SCRNABOX_PWD=/lustre04/scratch/fiorini9/scrna_pipeline/hto_v1.39/pipeline
+export SCRNABOX_HOME=/lustre04/scratch/fiorini9/scrna_pipeline/test_code/scrnabox0/scrnabox/scrnabox.slurm
+export SCRNABOX_PWD=/lustre04/scratch/fiorini9/scrna_pipeline/test_code/pipeline_hto
 
 bash $SCRNABOX_HOME/launch_scrnabox.sh \
 -d ${SCRNABOX_PWD} \
 --steps 1
-

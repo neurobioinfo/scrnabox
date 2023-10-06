5e7143d9957bca0538380a8cf23c5154951f1a91
fiorini9 <110110777+fiorini9@users.noreply.github.com>
2023-10-06 19:54:44 -0400

    Update hto_step5.R

--- scrnabox.slurm/hto/scripts/step5/hto_step5.R
+++ scrnabox.slurm/hto/scripts/step5/hto_step5.R
@@ -41,7 +41,7 @@ seu_list<-list()
 ################################################################################################################################################
 ## if users want to integrate Seurat objects
 ################################################################################################################################################
-if (tolower(par_skip_integration)=='no') {
+if (tolower(par_integrate_seurat)=='yes') {
 ## normalize exisiting Seurat objects
 seu_list<-foreach (i_s=1:length(sample_name)) %do% {  
     seu<-readRDS(paste(output_dir,'/step4/objs4/',sample_name[i_s], sep=""))
@@ -72,39 +72,39 @@ seu_int <- RunUMAP(seu_int, dims = 1:par_RunUMAP_dims, n.neighbors =par_RunUMAP_
 
 ## print PCA
 DimPlot(seu_int, reduction = "pca", group.by="MULTI_ID_Lables", raster = FALSE )
-ggsave(paste(output_dir,'/step5/figs5',"/DimPlot_pca.pdf", sep=""))
+ggsave(paste(output_dir,'/step5/figs5',"/integrated_DimPlot_pca.pdf", sep=""))
 
 ## print elbow plot
 ElbowPlot(seu_int)
-ggsave(paste(output_dir,'/step5/figs5',"/elbow.pdf", sep=""))
+ggsave(paste(output_dir,'/step5/figs5',"/integrated_elbow.pdf", sep=""))
 
 ## print UMAP
 DimPlot(seu_int, reduction = "umap", group.by="MULTI_ID_Lables", raster = FALSE)
-ggsave(paste(output_dir,'/step5/figs5',"/DimPlot_umap.pdf", sep=""))
+ggsave(paste(output_dir,'/step5/figs5',"/integrated_DimPlot_umap.pdf", sep=""))
 
 ## print Jack straw plot (optional)
 if (tolower(par_compute_jackstraw)=='yes') {
 seu <- JackStraw(seu, num.replicate = 100,dims = par_RunPCA_npcs) #added this figure
 seu <- ScoreJackStraw(seu, dims = 1:par_RunPCA_npcs)
 JackStrawPlot(seu, dims = 1:par_RunPCA_npcs)
-ggsave(paste(output_dir,'/step5/figs5',"/Jackstraw_plot.pdf", sep=""))
+ggsave(paste(output_dir,'/step5/figs5',"/integrated_Jackstraw_plot.pdf", sep=""))
 }
 
 ## save integrated Seurat object as RDS
 saveRDS(seu_int, paste(output_dir,'/step5/objs5',"/seu_step5.rds", sep=""))
 
 ## save metadata information
-write.csv(colnames(seu_int[[]]), paste(output_dir,'/step5/info5',"/meta_info_seu_step5.csv", sep=""))
+write.csv(colnames(seu_int[[]]), paste(output_dir,'/step5/info5',"/integrated_meta_info_seu_step5.csv", sep=""))
 
 ## save HTO counts
-write.csv(table(seu_int$MULTI_ID), paste(output_dir,'/step5/info5',"/MULTI_IDcount.csv", sep=""))
+write.csv(table(seu_int$MULTI_ID), paste(output_dir,'/step5/info5',"/integrated_MULTI_IDcount.csv", sep=""))
 }
 
 
 ################################################################################################################################################
 ## if users do not want to integrate Seurat objects (i.e. only have one Seurat object)
 ################################################################################################################################################
-if (tolower(par_skip_integration)=='yes') {
+if (tolower(par_one_seurat)=='yes') {
 ## normalize exisiting Seurat object
 seu_list<-foreach (i_s=1:length(sample_name)) %do% {  
     seu_int<-readRDS(paste(output_dir,'/step4/objs4/',sample_name[i_s], sep=""))
@@ -147,6 +147,72 @@ write.csv(colnames(seu_int[[]]), paste(output_dir,'/step5/info5',"/meta_info_seu
 ## save HTO counts
 write.csv(table(seu_int$MULTI_ID), paste(output_dir,'/step5/info5',"/MULTI_IDcount.csv", sep=""))
 }
+
+
+
+################################################################################################################################################
+## if users just want to merge their Seurat objects
+################################################################################################################################################
+if (tolower(par_merge_seurat)=='yes') {
+## normalize exisiting Seurat objects
+seu_list<-foreach (i_s=1:length(sample_name)) %do% {  
+    seu<-readRDS(paste(output_dir,'/step4/objs4/',sample_name[i_s], sep=""))
+    DefaultAssay(seu) <- par_DefaultAssay
+    seu <- Seurat::NormalizeData(seu,normalization.method = par_normalization.method,scale.factor =par_scale.factor)
+}  
+
+## merge Seurat objects
+length_list <- length(seu_list)
+
+vars <- seu_list[2:length_list]
+
+seu_int <- merge(seu_list[1], dput(as.character(vars)), add.cell.ids = dput(as.character(seu_list)), project = "merge",
+                         merge.data = TRUE)
+
+
+## set default assay to integrated
+Seurat::DefaultAssay(seu_int) <- "RNA"
+
+## find variable features
+seu_int<- FindVariableFeatures(seu_int, selection.method = par_selection.method, nfeatures = par_nfeatures)
+
+## scale integrated assay
+seu_int <- ScaleData(seu_int, verbose = FALSE)
+
+## run PCA and UMAP on integrated Seurat object
+seu_int <- RunPCA(seu_int, npcs = par_RunPCA_npcs, verbose = FALSE)
+seu_int <- RunUMAP(seu_int, dims = 1:par_RunUMAP_dims, n.neighbors =par_RunUMAP_n.neighbors)
+
+## print PCA
+DimPlot(seu_int, reduction = "pca", group.by="MULTI_ID_Lables", raster = FALSE )
+ggsave(paste(output_dir,'/step5/figs5',"/merge_DimPlot_pca.pdf", sep=""))
+
+## print elbow plot
+ElbowPlot(seu_int)
+ggsave(paste(output_dir,'/step5/figs5',"/merge_elbow.pdf", sep=""))
+
+## print UMAP
+DimPlot(seu_int, reduction = "umap", group.by="MULTI_ID_Lables", raster = FALSE)
+ggsave(paste(output_dir,'/step5/figs5',"/merge_DimPlot_umap.pdf", sep=""))
+
+## print Jack straw plot (optional)
+if (tolower(par_compute_jackstraw)=='yes') {
+seu <- JackStraw(seu, num.replicate = 100,dims = par_RunPCA_npcs) #added this figure
+seu <- ScoreJackStraw(seu, dims = 1:par_RunPCA_npcs)
+JackStrawPlot(seu, dims = 1:par_RunPCA_npcs)
+ggsave(paste(output_dir,'/step5/figs5',"/merge_Jackstraw_plot.pdf", sep=""))
+}
+
+## save Seurat object as RDS
+saveRDS(seu_int, paste(output_dir,'/step5/objs5',"/seu_step5.rds", sep=""))
+
+## save metadata information
+write.csv(colnames(seu_int[[]]), paste(output_dir,'/step5/info5',"/merge_meta_info_seu_step5.csv", sep=""))
+
+## save HTO counts
+write.csv(table(seu_int$MULTI_ID), paste(output_dir,'/step5/info5',"/merge_MULTI_IDcount.csv", sep=""))
+}
+
 
 ## save RNA expression matrix
 if (tolower(par_save_RNA)=='yes') {

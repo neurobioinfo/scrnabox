b8ddc729fa402d23c343dda71e3999ee181d5524
fiorini9 <110110777+fiorini9@users.noreply.github.com>
2023-10-06 18:37:18 -0400

    Update annotate.R

--- scrnaboxR/R/annotate.R
+++ scrnaboxR/R/annotate.R
@@ -1,37 +1,25 @@
-# Generate the annotation
-annotation<-function(level_cluster,ClusterMarkers,PWD,PSUE,top_sel,db) {
+# marker gene GSEA with enrichR
+annotation<-function(PWD,cluster_marker,db) {
   library(Seurat)
   library(dplyr)
   library(ggplot2)
-  library(xlsx)
   library(enrichR)
   setwd(PWD)
-  seu.q6 <- readRDS(PSUE)
-  ClusterMarkers<-readRDS(file = ClusterMarkers)
-  # mkdir -p annot_enrich
-  dir.create("annot_enrich")
-  for (i in sort(unlist(unique(seu.q6[[level_cluster]])))) {
+
+  ## need to read in the cluster marker csv file csv file
+  cluster_marker <- read.delim(cluster_marker, header = T, sep = ",") 
+  
+  
+  #create directories based on the clusters present in the csv file
+  dir.create("annot_enrich") 
+  for (i in unique(cluster_marker$cluster)) {
     dir.create(paste0(PWD,"/annot_enrich", "/clust",i))
   }
-  Idents(seu.q6) <- level_cluster
-  # ClusterMarkers <- FindAllMarkers(seu.q6, only.pos = TRUE)
-  # top5 <- ClusterMarkers %>% group_by(cluster) %>% top_n(n=top_sel, wt = avg_log2FC)
-  # write.csv(top5,"top_sel.csv")
-  # for (i in  sort(unlist(unique(seu.q6[[level_cluster]])))) {
-  #   N1.c0 <- ClusterMarkers %>% filter(cluster == i & avg_log2FC > 0)
-  #   genes <- N1.c0$gene
-  #   write.xlsx(genes, file="cluster_just_genes.xlsx",sheetName=paste0('cluster',i), row.names=FALSE,append=TRUE)
-  #   write.xlsx(N1.c0, file="cluster_whole.xlsx",sheetName=paste0('cluster',i), row.names=FALSE,append=TRUE)
-  # }
-  # heat_map<-DoHeatmap(seu.q6, features = top5$gene, size=3, angle =90, group.bar.height = 0.02, group.by = level_cluster)
-  # ggsave(file = "heatmap.pdf")
-  # dim_plot<-DimPlot(seu.q6,group.by = level_cluster, label = TRUE)
-  # ggsave(file = "umap.pdf")
-  setEnrichrSite("Enrichr") # Human genes
-  for (i in sort(unlist(unique(seu.q6[[level_cluster]])))) {
+
+  for (i in unique(cluster_marker$cluster)) {
     setwd(PWD)
     setwd(paste0('./annot_enrich/clust',i))
-    N1.c0 <- ClusterMarkers %>% filter(cluster == i & avg_log2FC > 0)
+    N1.c0 <- cluster_marker %>% filter(cluster == i & avg_log2FC > 0)
     genes <- N1.c0$gene
     N1.c0.Er <- enrichr(genes, databases = db)
     if(is.null(N1.c0.Er)) next

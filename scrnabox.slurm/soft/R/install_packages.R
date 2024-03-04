c1add48c17ba1bb0beca30a2b87d196c9d25ea39
saeidamiri1 <saeid.amiri1@gmail.com>
2024-03-04 13:45:57 -0500

    Built on 2024-02-28, v0.1.52.50

--- scrnabox.slurm/soft/R/install_packages.R
+++ scrnabox.slurm/soft/R/install_packages.R
@@ -13,34 +13,30 @@ R_LIB_PATH="~/scratch/R/x86_64-pc-linux-gnu-library/4.2";.libPaths(R_LIB_PATH)
 # R package requirements ----------------------------------------------------
 ## Installl devtool, if you are using the unix, make sure you already installed: build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
 install.packages("devtools", dependencies = TRUE)
-devtools::install_github("igraph/rigraph")
-devtools::install_github("tidyverse/ggplot2")
-devtools::install_github("tidyverse/dplyr")
-devtools::install_github("tidyverse/tidyverse")
-devtools::install_github('chris-mcginnis-ucsf/DoubletFinder')
-
-install.packages('tidyverse')
-install.packages('caTools')
-install.packages('gplots') 
-install.packages('ROCR')
-install.packages('vctrs')
-install.packages('fossil')
-install.packages('openxlsx')   
-install.packages('stringr') 
-install.packages('ggpubr')
-install.packages('data.table')
-install.packages('SoupX') 
-install.packages('ggrepel') 
-install.packages('scCustomize')
+require(devtools)
+install_version("Matrix", version = "1.6.0", repos = "http://cran.us.r-project.org")
+install_version("igraph", version = "1.5.0.1", repos = "http://cran.us.r-project.org")
+install_version("ggplot2", version = "3.4.2", repos = "http://cran.us.r-project.org")
+install_version("dplyr", version = "1.1.2", repos = "http://cran.us.r-project.org")
+install_version("foreach", version = "1.5.2", repos = "http://cran.us.r-project.org")
+install_version("doParallel", version = "1.0.17", repos = "http://cran.us.r-project.org")
+install_version("tidyverse", version ="2.0.0", repos = "http://cran.us.r-project.org", dependencies=FALSE)
+install_version("Seurat", "4.3.0.1", repos = "http://cran.us.r-project.org", dependencies=FALSE)
+install_version("SeuratObject", "4.1.3", repos = "http://cran.us.r-project.org", dependencies=FALSE)
+install_github('https://github.com/ekernf01/DoubletFinder', force = T)
+####################
+install_version("ggpubr" , version ="0.6.0", repos = "http://cran.us.r-project.org",dependencies=FALSE)
+install_version("SoupX" , version ="1.6.2", repos = "http://cran.us.r-project.org",dependencies=FALSE)
+install_version("xlsx" , version ="0.6.5", repos = "http://cran.us.r-project.org",dependencies=FALSE)
+install_version("data.table" , version ="1.14.8", repos = "http://cran.us.r-project.org",dependencies=FALSE)
+install.packages("BiocManager",version ="1.30.21.1",repos='https://cran.r-project.org')
 
 ### packages needed for scrnabox pipeline
-pkgs <- c('Seurat', 'foreach', 'doParallel', 'Matrix','cowplot','xlsx','clustree','enrichR','stringi','limma', 'edgeR', 
-'org.Hs.eg.db', 'SoupX','MatrixGenerics','BiocGenerics','S4Vectors','IRanges','GenomeInfoDb','GenomicRanges', 'Biobase', 
-'SummarizedExperiment', 'SingleCellExperiment', 'DropletUtils', 'EnhancedVolcano')
+pkgs <- c('cowplot','xlsx','clustree','enrichR','stringi','limma', 'edgeR', 'DESeq2','org.Hs.eg.db', 'SoupX','MatrixGenerics','BiocGenerics','S4Vectors','IRanges','GenomeInfoDb','GenomicRanges', 'Biobase', 'SummarizedExperiment', 'SingleCellExperiment', 'DropletUtils', 'EnhancedVolcano','MAST')
 
 ## install Bioconductor 
 if (!require("BiocManager", quietly = TRUE)) {
-  install.packages("BiocManager")
+  install.packages("BiocManager",version ="1.30.21.1",repos='https://cran.r-project.org', dependencies=FALSE)
 }
 
 ## install and check package loading
@@ -56,4 +52,4 @@ for (pkg in basename(pkgs)) {
 }
 
 ## Install scrnabox
-devtools::install_github("neurobioinfo/scrnabox/scrnaboxR")
+devtools::install_github("neurobioinfo/scrnabox/scrnaboxR",dependencies=FALSE)
